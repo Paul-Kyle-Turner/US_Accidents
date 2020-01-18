d5ef076498d9860d682ac1f6066b3a2c1c43f631
John Bucknam <jbuck594@gmail.com>
2020-01-17 20:58:44 -0500

    Updated Header Information
    Header Information
    ==================
    I converted each element in the header list from a string describing
    each column  to a tuple containing the said string and the type the
    column uses.
    
    Example: ("Start_Lat", float)

--- header.py
+++ header.py
@@ -5,67 +5,80 @@ header strings.
 
 '''Header Listing
 This constant listing gives the header for each column in the US Accidents data.
+Each header element is a tuple consisting of the header string and the type used for that column.
 '''
-HEADER = ['ID',
-          'Source', 
-          'TMC', 
-          'Severity', 
-          'Start_Time', 
-          'End_Time', 
-          'Start_Lat', 
-          'Start_Lng', 
-          'End_Lat', 
-          'End_Lng', 
-          'Distance(mi)', 
-          'Description', 
-          'Number', 
-          'Street', 
-          'Side', 
-          'City', 
-          'County', 
-          'State', 
-          'Zipcode', 
-          'Country', 
-          'Timezone', 
-          'Airport_Code', 
-          'Weather_Timestamp', 
-          'Temperature(F)', 
-          'Wind_Chill(F)', 
-          'Humidity(%)', 
-          'Pressure(in)', 
-          'Visibility(mi)', 
-          'Wind_Direction', 
-          'Wind_Speed(mph)', 
-          'Precipitation(in)', 
-          'Weather_Condition', 
-          'Amenity', 
-          'Bump', 
-          'Crossing', 
-          'Give_Way', 
-          'Junction', 
-          'No_Exit', 
-          'Railway', 
-          'Roundabout', 
-          'Station', 
-          'Stop', 
-          'Traffic_Calming', 
-          'Traffic_Signal', 
-          'Turning_Loop', 
-          'Sunrise_Sunset', 
-          'Civil_Twilight', 
-          'Nautical_Twilight', 
-          'Astronomical_Twilight']
+HEADER = [('ID', str), 
+          ('Source', str), 
+          ('TMC', float), 
+          ('Severity', int), 
+          ('Start_Time', str), 
+          ('End_Time', str), 
+          ('Start_Lat', float), 
+          ('Start_Lng', float), 
+          ('End_Lat', float), 
+          ('End_Lng', float), 
+          ('Distance(mi)', float), 
+          ('Description', str), 
+          ('Number', float), 
+          ('Street', str), 
+          ('Side', str), 
+          ('City', str), 
+          ('County', str), 
+          ('State', str), 
+          ('Zipcode', str), 
+          ('Country', str), 
+          ('Timezone', str), 
+          ('Airport_Code', str), 
+          ('Weather_Timestamp', str), 
+          ('Temperature(F)', float), 
+          ('Wind_Chill(F)', float), 
+          ('Humidity(%)', float), 
+          ('Pressure(in)', float), 
+          ('Visibility(mi)', float), 
+          ('Wind_Direction', str), 
+          ('Wind_Speed(mph)', float), 
+          ('Precipitation(in)', float), 
+          ('Weather_Condition', str), 
+          ('Amenity', bool), 
+          ('Bump', bool), 
+          ('Crossing', bool), 
+          ('Give_Way', bool), 
+          ('Junction', bool), 
+          ('No_Exit', bool), 
+          ('Railway', bool), 
+          ('Roundabout', bool), 
+          ('Station', bool), 
+          ('Stop', bool), 
+          ('Traffic_Calming', bool), 
+          ('Traffic_Signal', bool), 
+          ('Turning_Loop', bool), 
+          ('Sunrise_Sunset', str), 
+          ('Civil_Twilight', str), 
+          ('Nautical_Twilight', str), 
+          ('Astronomical_Twilight', str)]
 
 __all__ = ["HEADER"]
 
 
+# Header Fetch Script
 if __name__ == "__main__":
     import pandas as pd
     
     def gather_data(file="data/US_Accidents_May19.csv"):
         with open(file, 'r+') as csv_file:
-            data = pd.read_csv(csv_file, sep=',', nrows=2)
-        return data.columns.values, data
+            data = pd.read_csv(csv_file, sep=',')
+        return data.columns.values, data.to_numpy()
 
-    header, _ = gather_data()
+    header, data = gather_data()
+
+    # Print Header Strings
     print(header.tolist())
+
+    types = []
+    for col in data.T:
+        types.append(type(col[0]))
+    
+    # Print Data Types for each Column/Header
+    print(types)
+
+    print(list(map(lambda x, y: (x, y), header, types)))
